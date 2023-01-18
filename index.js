1dc7888219f8b9be5aaa65284b0e794e3c0d7747
Manaheel jamil <96910538+ManaheelJamil@users.noreply.github.com>
2023-01-18 20:33:21 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+import chalkAnimation from 'chalk-animation';
+const sleep = () => {
+    return new Promise((res) => {
+        setTimeout(res, 3000);
+    });
+};
+async function welcome() {
+    let rainbow = chalkAnimation.rainbow('NUMBER GUESSING GAME');
+    await sleep();
+    // rainbow.stop();
+}
+await welcome();
+const num = Math.ceil(Math.random() * 4);
+console.log(num);
+const game = await inquirer.prompt([{
+        name: "random",
+        type: "number",
+        message: "Select A number between 1 to 10"
+    }]);
+if (game.random == num) {
+    console.log(chalk.blueBright("you guess the right number"));
+}
+else {
+    console.log(chalk.red("opss!!! you guess wrong number"));
+}
+// console.log(num)
